de17d8ceac7e8b0442d169da619ca92ab2af45e9
Utsugi0101 <soraki0101@gmail.com>
2026-01-26 04:06:53 +0900

    Reactプロジェクト作成

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <main className="app">
+      <h1>BANSOU Test</h1>
+      <p>Vite + React の最小構成です。</p>
+      <ul>
+        <li>npm install</li>
+        <li>npm run dev</li>
+      </ul>
+    </main>
+  );
+}
