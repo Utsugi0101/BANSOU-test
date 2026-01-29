a0580a89f97cfeae8dba11b2c334f8506ea37e41
Utsugi0101 <soraki0101@gmail.com>
2026-01-30 07:53:17 +0900

    もとに戻した

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [text, setText] = useState('SecHack365')
+  const [text, setText] = useState('HelloWorld！！')
 
   return (
     <div className="app">
